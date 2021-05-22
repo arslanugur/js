88802d572e4cbe34bbee15de17dc56678842cbab
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-22 21:29:09 +0300

    Create your-first-program.js

---
+++ Overview/your-first-program.js
@@ -0,0 +1,10 @@
+Your First Program
+      Write a program to print "JS is fun".
+      Note that the sentence starts with capital letters.
+
+      Hint
+      Use console.log() function.
+      Remember to enclose the text into double quotes.
+
+      JS Code:
+ console.log("JS is fun")
