f83d98babe2be6b0e1595167125befeb888ad385
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-23 07:16:06 +0300

    Create comments.js

---
+++ Overview/comments.js
@@ -0,0 +1,30 @@
+JavaScript Comments
+      We know about statements, these are the instructions within our program that get "executed" when the program runs.
+      But! Not all JavaScript statements are "executed".
+      Any code after a double slash //, or between /* and */, is treated as a comment, and will be ignored, and not executed.
+
+      To write a Single line comment we use double slashes. Like this:
+// This is a single line comment
+alert("This is an alert box!");
+
+      But why write code that is never going to be executed. Isn’t that a waste of time?
+      Not at all! 
+      Comments are a good idea, especially ones relating to large functions, as they help make our code more readable for others. 
+
+      alert() is used to create a message box.
+      
+Multiple-Line Comments
+      What if we have more to say?
+
+      If we want to create a multi-line comment, we write it between /*and */
+      Like this:
+/* This code 
+   creates an 
+   alert box */
+alert("This is an alert box!");
+
+      We use comments to describe and explain what the code is doing.
+      
+      
+      
+      
