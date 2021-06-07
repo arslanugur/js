cf5447d4b9055f5548a49054ca7ea855bf3d5772
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-07 23:45:17 +0300

    Create assignment-operators.js

---
+++ Basic Concepts/assignment-operators.js
@@ -0,0 +1,25 @@
+Assignment Operators
+      Next in a series very logically named operators is Assignment operators!
+      And you guessed it, we use these guys to assign values to JavaScript variables. 
+      
+Operator|Example |Is equivalent to
+-----------------------------------
+=       |x = y   |x = y
++=      |x += y  |x = x + y
+-=      |x -= y  |x = x - y
+*=      |x *= y  |x = x * y
+/=      |x /= y  |x = x / y
+%=      |x %= y  |x = x % y
+
+      You can use multiple assignment operators in one line, such as x -= y += 9.
+      
+var result = 20;
+result *= 5;
+
+      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#assignment_operators
+      
+      
+      
+      
+      
+      
