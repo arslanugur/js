62da8a78ecb84534f8d3c5ce10ab3bfe56ade788
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-08 03:25:19 +0300

    Create practice-even-numbers.js

---
+++ Overview/practice-even-numbers.js
@@ -0,0 +1,14 @@
+      Example:
+for (var i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        alert(i);
+    }
+}
+
+      Another Example:
+var x =0;
+for(;x <=30;x +=2){
+  document.write(x +"<b1 />");
+}
+
+
