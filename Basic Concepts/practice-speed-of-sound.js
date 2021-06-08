f2d41d8cf5a8860c65d5a058345efe4b4d24db9f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-08 03:19:47 +0300

    Create practice-speed-of-sound.js

---
+++ Basic Concepts/practice-speed-of-sound.js
@@ -0,0 +1,5 @@
+var time = prompt("enter the time in seconds: ");
+var speed = prompt("enter the speed of the wind in meter");
+var distance = speed * time;
+document.write("The distance covered by the sound in meter is : " + distance);
+
