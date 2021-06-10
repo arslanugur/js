f807249ade93b7af1fde3555acf979063e784c6d
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-10 06:00:11 +0300

    Update string-operators.js

--- Basic Concepts/string-operators.js
+++ Basic Concepts/string-operators.js
@@ -1,13 +1,13 @@
 String Operators
-      Concatenation:
-      We can use concatenation (represented by the + sign) to build strings made up of multiple smaller strings, 
-      or by joining strings with other types. Check it out:
+      //Concatenation:
+      //We can use concatenation (represented by the + sign) to build strings made up of multiple smaller strings, 
+      //or by joining strings with other types. Check it out:
 var mystring1 = "I am learning ";
 var mystring2 = "JavaScript.";
 document.write(mystring1 + mystring2);
 
-      Numbers in quotes are treated as strings: 
-      So "42" is not the number 42, it’s a string that includes the two separate characters, 4 and 2. 
+      //Numbers in quotes are treated as strings: 
+      //So "42" is not the number 42, it’s a string that includes the two separate characters, 4 and 2. 
 
 var first=7; 
 var second=8; 
@@ -21,6 +21,10 @@ var x = "50";
 var y = "100";
 document.write(x+y); //output: 50100
 
-Which of these names are acceptable for JavaScript variables? 
+      //Which of these names are acceptable for JavaScript variables? 
   _module //Underscore is allowed
   firstNumber
+
+  
+  
+  
