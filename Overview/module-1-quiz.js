2d1257c5e472aec7825911b29168025b0bb724da
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-10 05:45:09 +0300

    Update module-1-quiz.js

--- Overview/module-1-quiz.js
+++ Overview/module-1-quiz.js
@@ -1,16 +1,16 @@
-      Declare a variable called x, assign the value 42 to it and output it to the console.
+      //Declare a variable called x, assign the value 42 to it and output it to the console.
 x = 42;
 console.log(x);
 
 
-      What is the output of this code?
+      //What is the output of this code?
 //x=8;
 x=2;
 //x=3;
 console.log(x);       //output: 2
 
 
-      to form valid JavaScript code that declares a variable and outputs it to the console.
+      //to form valid JavaScript code that declares a variable and outputs it to the console.
 <script>
 var name = "James";
 console.log(name);
