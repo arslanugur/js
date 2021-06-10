249009b669e08323718c639ae3310ee28d44b22e
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-10 05:53:14 +0300

    Update demo-trip-planner.js

--- Basic Concepts/demo-trip-planner.js
+++ Basic Concepts/demo-trip-planner.js
@@ -1,13 +1,13 @@
 Trip Planner
-      You need to plan a road trip. You are traveling at an average speed of 40 miles an hour.
-      Given a distance in miles as input (the code to take input is already present), 
-      output to the console the time it will take you to cover it in minutes.
-
+      //You need to plan a road trip. You are traveling at an average speed of 40 miles an hour.
+      //Given a distance in miles as input (the code to take input is already present), 
+      //output to the console the time it will take you to cover it in minutes.
+/*
 Sample Input:
 100, 150, 590
 Sample Output:
 150, 225, 885
-
+*/
       node.js Code:
 function main() {
     var distance = parseInt(readLine(), 10);
@@ -16,10 +16,10 @@ function main() {
     console.log(minutes);
 }
 
-      Explanation: It will take 150/40 = 3.75 hours to cover the distance, which is equivalent to 3.75*60 = 225 minutes. 
+      //Explanation: It will take 150/40 = 3.75 hours to cover the distance, which is equivalent to 3.75*60 = 225 minutes. 
       
       
       
       
       
-      
+   
