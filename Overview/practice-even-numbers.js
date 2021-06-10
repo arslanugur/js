cf43bb50fa812a3318fb26b01b4d965c1f53280f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-10 05:45:33 +0300

    Update practice-even-numbers.js

--- Overview/practice-even-numbers.js
+++ Overview/practice-even-numbers.js
@@ -1,11 +1,11 @@
-      Example:
+      //Example:
 for (var i = 2; i <= 10; i++) {
     if (i % 2 == 0) {
         alert(i);
     }
 }
 
-      Another Example:
+      //Example:
 var x =0;
 for(;x <=30;x +=2){
   document.write(x +"<b1 />");
