77198a7723a1cfdebbd50993cb34077ff894ff15
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-06-11 23:18:36 +0300

    Create while-loop.js

---
+++ Conditionals and Loops/while-loop.js
@@ -0,0 +1,10 @@
+The While Loop
+      //Time to move on to the second of our three loop statements, while.
+
+      //The while loop repeats through a block of code, but only as long as a specified condition is true.
+
+      //Here's the syntax:
+while (condition) {    
+   code block
+}
+      //The condition can be any conditional statement that returns true or false.
