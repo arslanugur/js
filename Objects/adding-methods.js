9efdca23e8471402880969c1850ae3bd0a763b69
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-07-21 16:09:27 +0300

    Update adding-methods.js

--- Objects/adding-methods.js
+++ Objects/adding-methods.js
@@ -62,3 +62,103 @@ function bornYear() {
       //As you can see, we have assigned the object's yearOfBirth property to the bornYear function.
       //The this keyword is used to access the age property of the object, which is going to call the method.
 
+      //Note that it's not necessary to write the function's parentheses when assigning it to an object.
+ 
+      //Example:
+function person(name, age) { 
+      this.name= name; 
+      this.age = age; 
+      this.yearOfBirth = bornYear; } 
+
+function bornYear() { 
+      return 2016 - this.age; } 
+
+var p = new person("JJ",21); 
+document.write(p.yearOfBirth());
+
+
+      //Example: Associate the "testData" constructor function below with a method called "mymethod":
+function testData (first, second) {
+  this.first = first;
+  this.second = second;
+  this.checkData = mymethod;
+}
+
+
+      // Example: Call the method as usual
+function person(name, age) {
+    this.name= name;
+    this.age = age;
+    this.yearOfBirth = bornYear;
+}
+function bornYear() {
+    return 2016 - this.age;
+}
+
+var p = new person("A", 22);
+
+document.write(p.yearOfBirth()); //output: 1994
+
+      //Call the method by the property name 
+      //you specified in the constructor function, rather than the function name.
+
+      //Example:
+function person(name, yearOfBirth) { 
+      this.name= name; 
+      this.yearOfBirth = yearOfBirth; 
+      this.age = ageNow; } 
+function ageNow() { return 2017 - this.yearOfBirth; } 
+var p = new person("A", 1993); 
+document.write(p.age()); //The output is 24
+
+
+      //Example:
+function person(name, age) {
+    this.name= name;
+    this.age = age;
+    this.yearOfBirth = bornYear;
+}
+var currentTime = new Date()
+var year = currentTime.getFullYear()
+function bornYear() {
+    return year - this.age;
+}
+
+var p = new person("A", prompt("Type your age"));
+
+document.write("This year is "+year+", and you were born in "+p.yearOfBirth()); 
+
+      //Example: View adult content 
+function person(name, year) {
+    this.name= name;
+    this.yearOfBirth  = year;
+    this.age = calculateAge;
+}
+function calculateAge() {
+    return 2017 - this.yearOfBirth ;
+}
+
+//request user to input year of birth
+var inputYear = prompt("Enter Year of Birth");
+
+var p = new person("James", inputYear);
+
+//checks if user is an adult
+if(p.age()<18){
+    alert("Explicit Content! Acess Denied");
+    document.write("You are under age..go back");
+}
+else{
+    alert("Access Allowed");
+    document.write("View our exciting adult content")
+    
+}
+
+      //In order to use the object's properties within a function, use: the "this" keyword
+
+
+
+
+
+
+
