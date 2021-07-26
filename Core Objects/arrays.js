5f5c80155612b1d257a762bb5b8a842e8f1c6e50
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-07-26 17:19:30 +0300

    Update arrays.js

--- Core Objects/arrays.js
+++ Core Objects/arrays.js
@@ -1,1 +1,16 @@
+Arrays
+      //Arrays store multiple values in a single variable.
 
+      //To store three course names, you need three variables.
+
+      //Example:
+var course1 ="HTML"; 
+var course2 ="CSS"; 
+var course3 ="JS"; 
+
+      //But what if you had 500 courses? The solution is an array
+var courses = new Array("HTML", "CSS", "JS"); 
+
+      //This syntax declares an array named courses, which stores three values, or elements.
+
+comments
