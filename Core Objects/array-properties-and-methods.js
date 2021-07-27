1c19e4c548ef78e6af45c0bfa68a448659865678
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-07-27 17:19:11 +0300

    Update array-properties-and-methods.js

--- Core Objects/array-properties-and-methods.js
+++ Core Objects/array-properties-and-methods.js
@@ -1,1 +1,19 @@
+The length Property
+      //JavaScript arrays have useful built-in properties and methods.
+      //An array's length property returns the number of its elements.
+var courses = ["HTML", "CSS", "JS"];
+document.write(courses.length); //3
 
+      //If you want to print the whole array: 
+var courses = ["HTML", "CSS", "JS"]; 
+var x = courses.length; 
+for (var i=0; i"); };
+     
+     //Example:
+var courses = ["HTML", "CSS", "JS"]; 
+document.write(courses.length); 
+document.write(courses[0].length); 
+document.write(courses[1].length); 
+document.write(courses[2].length);
+
+     
