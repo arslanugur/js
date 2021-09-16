8543e4530aaa3cc13d5a7852d605eddfdd44cfdd
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-09-17 02:23:26 +0300

    Update selecting-elements.js

---
+++ DOM & Events/selecting-elements.js
@@ -0,0 +1,10 @@
+Selecting Elements
+      //All HTML elements are objects. And as we know every object has properties and methods.
+      //The document object has methods that allow you to select the desired HTML element.
+      //These three methods are the most commonly used for selecting HTML elements: 
+
+document.getElementById(id) //finds element by id
+
+document.getElementsByClassName(name) //finds elements by class name
+
+document.getElementsByTagName(name) //finds elements by tag name
