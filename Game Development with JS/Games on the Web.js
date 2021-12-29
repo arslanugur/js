0210ded7947a565c4ffac3ccebdf41a3f72d7ab9
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-30 02:28:53 +0300

    Create Games on the Web.js

---
+++ Game Development with JS/Games on the Web.js
@@ -0,0 +1,100 @@
+// JavaScript is a programming language for the web.
+
+// It can be used to create interactive elements, build drop down menus, handle button click events, and much more!
+// In our course we will use JavaScript to draw shapes, graphics as well as to create animations.
+
+
+// Benefits of HTML
+// This is possible because it runs in the browser, like a simple website.
+// Most modern browsers support the functionality that we will use, making our games run even on mobile devices!
+
+/* 
+Here are some of the things we will learn in this course:
+- HTML
+- CSS
+- JavaScript basics
+- Drawing on the web
+- Animations
+- Game Loops
+- Collision Detection
+- & much more! */
+
+
+
+// HTML Basics
+  // HTML stands for HyperText Markup Language. A markup language uses tags to identify content.
+  // Here is an example of an HTML tag:
+<p> I'm a paragraph </p>
+  // <p> is the tag used to create a paragraph of text. 
+
+// Tags
+  // Most tags have an opening and a closing tag, with the content being placed between them.
+  // For example, the <p> paragraph tag needs to have a closing tag </p>. 
+  // The closing tag is created using a forward slash /.
+  // <Opening Tag> Content </Closing Tag>
+
+// to create 'Basic HTML Document Structure'
+  // There are many different tags in HTML that serve their purpose.
+  // A basic HTML document on the web has the following structure: 
+<html>
+   <body>
+   </body>
+</html>
+
+  // The <html> tag defines the page, 
+  // while the <body> tag defines the main content of the HTML document and contains all the elements of the page, such as paragraphs, tables, lists, etc.
+
+
+// HTML Headings
+  // Now, let's look at some of the basic elements that we can create.
+  // Headings are used to create titles and sub-titles on a page.
+  // HTML includes six levels of headings, which are ranked according to importance.
+  // These are <h1>, <h2>, <h3>, <h4>, <h5>, and <h6>.
+
+  // The following code defines all of the headings:
+<html>
+    <head>
+        <title>first page</title>
+    </head>
+    <body>
+        <h1>This is heading 1</h1>  # The Largest Heading
+        <h2>This is heading 2</h2>
+        <h3>This is heading 3</h3>
+        <h4>This is heading 4</h4>
+        <h5>This is heading 5</h5>
+        <h6>This is heading 6</h6>
+    </body>
+</html>
+  // The most used headings are h1, h2 and h3. Don't use headings to make the text big or bold. 
+  // Search engines use the headings to index the structure and content of your web pages. So use it wisely
+
+
+// Paragraphs
+  // As we have seen before, paragraphs are created using the <p> tag.
+  // We can have multiple <p> tags on our page: 
+<h1>My Game</h1>
+<p>This is fun</p>
+<p>This is another text</p>
+
+
+// Line Break
+  // Some HTML elements have no closing tag.
+  // For example, the <br /> tag is used to add a line break: The above code creates a paragraph of text that includes a line
+<p>
+Name: Player 1 <br />
+Points: 42
+</p>
+  // The above code creates a paragraph of text that includes a line break. 
+  // As you can see, HTML tags can be included in other tags to format the content.
+  
+  // Other examples of common self closing tags: <hr>, <img>, <link> and <area>
+
+  // How many lines of text will the following code create? -- 3
+<p>
+One <br />
+Two <br />
+Three
+</p>
+
+
+
