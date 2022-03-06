db7ae03efa45d422fc8684c7482f656961e3d15d
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-06 11:26:09 +0200

    Create 01. Add Two Numbers.js

---
+++ Examples/01. Introduction/01. Add Two Numbers.js
@@ -0,0 +1,55 @@
+// Introduction Examples
+// Example: to add two numbers and display their sum using various methods in JavaScript.
+// Topics: JS Variables and Constants, JS Operators
+
+// We use the + operator to add two or more numbers.
+// Example 1: Add Two Numbers
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+// Output: The sum of 5 and 3 is: 8
+
+
+// Example 2: Add Two Numbers Entered by the User
+
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+/*
+Output:
+Enter the first number 5
+Enter the second number 3
+The sum of 5 and 3 is: 8
+*/
+
+// The above program asks the user to enter two numbers. Here, prompt() is used to take inputs from the user. parseInt() is used to convert the user input string to number.
+
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+// Then, the sum of the numbers is computed.
+const sum = num1 + num2;
+
+// Finally, the sum is displayed. To display the result, we have used the template literal ` `. This allows us to include variables inside strings.
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+// To include variables inside ``, you need to use the ${variable} format.
+
+// Note: Template literals was introduced in ES6 and some browsers may not support them. 
+// To learn more, visit, JavaScript template literals support. https://caniuse.com/#feat=template-literals
+
+
+
