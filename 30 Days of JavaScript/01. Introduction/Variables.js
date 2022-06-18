a4f822a271ee6d9f1c3e1dceff0b8e4863ea34f3
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-18 22:02:33 +0300

    Create Variables.js

---
+++ 30 Days of JavaScript/01. Introduction/Variables.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'Ugur'        // first name of a person
+let lastName = 'Arslan'       // last name of a person
+let country = 'Turkey'        // country
+let city = 'Ankara'           // capital city
+let age = 100                 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Arslan',     //name of a person
+  job = 'teacher',
+  live = 'Turkey'
