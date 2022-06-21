f15c2398fb0ef1449d36b7d620b5fc2e54e382f2
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-21 17:53:03 +0300

    Update String Methods.js

--- 30 Days of JavaScript/02. Data Types/String Methods.js
+++ 30 Days of JavaScript/02. Data Types/String Methods.js
@@ -33,3 +33,82 @@ let lastIndex = string.length - 1
 console.log(string.charCodeAt(lastIndex)) // t ASCII is 116
 
 
+
+// 04. Concat()
+// concat(): it takes many substrings and creates concatenation.
+// string.concat(substring, substring, substring)
+let string = '30'
+console.log(string.concat("Days", "Of", "JavaScript")) // 30DaysOfJavaScript
+let country = 'Fin'
+console.log(country.concat("land")) // Finland
+
+
+// 05. endsWith()
+// endsWith: it takes a substring as an argument and it checks if the string starts with that specified substring. It returns a boolean(true or false).
+// string.endsWith(substring)
+let string = 'Love is the best to in this world'
+console.log(string.endsWith('world')) // true
+console.log(string.endsWith('love')) // false
+console.log(string.endsWith('in this world')) // true
+
+let country = 'Finland'
+console.log(country.endsWith('land')) // true
+console.log(country.endsWith('fin')) // false
+console.log(country.endsWith('Fin')) //  false
+
+
+
+// 06. includes()
+// includes(): It takes a substring argument and it check if substring argument exists in the string. includes() returns a boolean. It checks if a substring exist in a string and it returns true if it exists and false if it doesn't exist.
+let string = '30 Days Of JavaScript'
+console.log(string.includes('Days'))     // true
+console.log(string.includes('days'))     // false
+console.log(string.includes('Script'))     // true
+console.log(string.includes('script'))     // false
+console.log(string.includes('java'))     // false
+console.log(string.includes('Java'))     // true
+
+let country = 'Finland'
+console.log(country.includes('fin')) // false
+console.log(country.includes('Fin')) // true
+console.log(country.includes('land')) // true
+console.log(country.includes('Land')) // false
+
+
+
+
+// 07. indexOf()
+// indexOf(): Takes takes a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
+
+string.indexOf(substring)
+let string = '30 Days Of JavaScript'
+console.log(string.indexOf('D'))          // 3
+console.log(string.indexOf('Days'))       // 3
+console.log(string.indexOf('days'))       // -1
+console.log(string.indexOf('a'))          // 4
+console.log(string.indexOf('JavaScript')) // 11
+console.log(string.indexOf('Script'))     // 15
+console.log(string.indexOf('script'))     // -1
+
+
+
+
+// 08. lastIndexOf()
+// lastIndexOf(): Takes takes a substring and if the substring exists in a string it returns the last position of the substring if it does not exist it returns -1
+
+let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log(string.lastIndexOf('love'))       // 67
+console.log(string.lastIndexOf('you'))        // 63
+console.log(string.lastIndexOf('JavaScript')) // 38
+
+
+
+
+// 09. 
+
+
+
+
+
+
+
