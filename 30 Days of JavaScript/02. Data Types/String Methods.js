778b8c7c68e092bdd5db59ec19ed592c6b211b11
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-23 14:41:34 +0300

    Update String Methods.js

--- 30 Days of JavaScript/02. Data Types/String Methods.js
+++ 30 Days of JavaScript/02. Data Types/String Methods.js
@@ -104,8 +104,98 @@ console.log(string.lastIndexOf('JavaScript')) // 38
 
 
 
-// 09. 
+// 09. length()
+// length: The string length method returns the number of characters in a string included empty space. Example:
 
+let js = 'JavaScript'
+console.log(js.length)        // 10
+let firstName = 'Asabeneh'
+console.log(firstName.length) // 8
+
+
+// 10. match()
+// match: it takes a substring or regular expression pattern as an argument and it returns an array if there is match if not it returns null. Let us see how a regular expression pattern looks like. It starts with / sign and ends with / sign.
+let string = 'love'
+let patternOne = /love/ // with out any flag
+let patternTwo = /love/gi // g-means to search in the whole text, i - case insensitive
+string.match(substring)
+let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log(string.match('love')) //
+/*
+output
+["love", index: 2, input: "I love JavaScript. If you do not love JavaScript what else can you love.", groups: undefined]
+*/
+let pattern = /love/gi
+console.log(string.match(pattern)) // ["love", "love", "love"]
+// Let us extract numbers from text using regular expression. This is not regular expression section, no panic.
+
+let txt = 'In 2019, I run 30 Days of Python. Now, in 2020 I super exited to start this challenge'
+let regEx = /\d/g // d with escape character means d not a normal d instead acts a digit
+// + means one or more digit numbers, 
+// if there is g after that it means global, search everywhere.
+console.log(txt.match(regEx)) // ["2", "0", "1", "9", "3", "0", "2", "0", "2", "0"]
+console.log(txt.match(/\d+/g)) // ["2019", "30", "2020"]
+
+
+
+// 11. repeat()
+// repeat(): it takes a number argument and it returned the repeated version of the string.
+// string.repeat(n)
+let string = 'love'
+console.log(string.repeat(10)) // lovelovelovelovelovelovelovelovelovelove
+
+
+
+// 12. replace()
+// replace(): takes to parameter the old substring and new substring.
+// string.replace(oldsubstring, newsubstring)
+
+let string = '30 Days Of JavaScript'
+console.log(string.replace('JavaScript', 'Python')) // 30 Days Of Python
+let country = 'Finland'
+console.log(country.replace('Fin', 'Noman')) // Nomanland
+
+
+
+// 13. search()
+// search: it takes a substring as an argument and it returns the index of the first match.
+// string.search(substring)
+let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+console.log(string.search('love')) // 2
+
+
+
+// 14. split()
+// split(): The split method splits a string at a specified place.
+let string = '30 Days Of JavaScript'
+console.log(string.split())     // ["30 Days Of JavaScript"]
+console.log(string.split(' '))  // ["30", "Days", "Of", "JavaScript"]
+let firstName = 'Asabeneh'
+console.log(firstName.split())  // ["Asabeneh"]
+console.log(firstName.split(''))  // ["A", "s", "a", "b", "e", "n", "e", "h"]
+let countries = 'Finland, Sweden, Norway, Denmark, and Iceland'
+console.log(countries.split(',')) // ["Finland", " Sweden", " Norway", " Denmark", " and Iceland"]
+console.log(countries.split(', '))   //  ["Finland", "Sweden", "Norway", "Denmark", "and Iceland"]
+
+
+
+// 15. startWith()
+// startsWith: it takes a substring as an argument and it checks if the string starts with that specified substring. It returns a boolean(true or false).
+// string.startsWith(substring)
+let string = 'Love is the best to in this world'
+console.log(string.startsWith('Love')) // true
+console.log(string.startsWith('love')) // false
+console.log(string.startsWith('world')) // false
+
+let country = 'Finland'
+console.log(country.startsWith('Fin')) // true
+console.log(country.startsWith('fin')) // false
+console.log(country.startsWith('land')) //  false
+
+
+
+
+// 16. 
 
 
 
