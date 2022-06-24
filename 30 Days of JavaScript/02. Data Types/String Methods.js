8e6e1c5d64bc8c660891fbc376ee9d0e6e9860af
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-24 15:33:49 +0300

    Update String Methods.js

--- 30 Days of JavaScript/02. Data Types/String Methods.js
+++ 30 Days of JavaScript/02. Data Types/String Methods.js
@@ -195,10 +195,28 @@ console.log(country.startsWith('land')) //  false
 
 
 
-// 16. 
+// 16. substr()
+//substr(): It takes two arguments,the starting index and number of characters to slice.
+let string = 'JavaScript'
+console.log(string.substr(4,6))    // Script
+let country = 'Finland'
+console.log(country.substr(3, 4))   // land
 
 
 
+// 17. substring()
+// substring(): It takes two arguments,the starting index and the stopping index but it doesn't include the stopping index.
+let string = 'JavaScript'
+console.log(string.substring(0,4))   // Java
+console.log(string.substring(4,10))     // Script
+console.log(string.substring(4))    // Script
+let country = 'Finland'
+console.log(country.substring(0, 3))   // Fin
+console.log(country.substring(3, 7))   // land
+console.log(country.substring(3))   // land
 
+
+
+// 18. 
 
 
