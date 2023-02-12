85c814a6abd7aa4defdd98183f16c92ae68b568c
Arslan <45125597+arslanugur@users.noreply.github.com>
2023-02-12 14:42:17 +0300

    Update Methods.js

--- Methods.js
+++ Methods.js
@@ -166,12 +166,13 @@ array[array.length] = 4;
 
 
 // JS NUMBER METHODS
+// All number methods return a new value. They do not change the original value.
 // Part I: Converting Variables to Num & String
 toString()
 Number()
 parseFloat()
 parseInt()
-// Part II: JS Number Methods
+// Part II: Number Methods
 toExponential()
 toFixed()
 toPrecision()
@@ -183,7 +184,81 @@ number.parseFloat()
 number.parseInt()
 
 
+// Part I: Converting Variables to Num & String
+// 01. toString()
+// The toString() method returns a number as a string.
 
+// Syntax
+number.toString(radix);
+
+// Example
+let num = 24;
+let str = num.toString();
+console.log(str, typeof str); // 24, string
+
+------------------------
+
+// 02. Number()
+// The Number() method can be used to convert JS variables to numbers,
+// If the number cannot be converted, NaN (Not a Number) is returned.
+
+// Syntax
+Number(value);
+
+// Examples
+let str = "Code";
+
+console.log(Number(str));       // NaN
+console.log(Number(true));      // 1
+console.log(Number(false));     // 0
+console.log(Number("10"));      // 10
+console.log(Number(" 10"));     // 10
+console.log(Number("10 "));     // 10
+console.log(Number(" 10 "));    // 10
+console.log(Number("10.33"));   // 10.33
+console.log(Number("10, 33"));  // NaN
+console.log(Number("10 33") );  // NaN
+console.log(Number("Code"));    // NaN
+
+------------------------
+// 03. parseFloat()
+// The Number.parseFloat() method parses a value as a string and returns the first number.
+// If the first character cannot be converted, NaN is returned, Leading and trailing spaces are ignored.
+// Only the first number found is returned.
+
+// Syntax
+Number.parseFloat(value);
+
+// Examples
+Number.parseFloat(10);          // 10
+Number.parseFLoat("10");        // 10
+Number.parseFloat("10.33");     // 10.33
+Number.parseFloat("45 66");     // 45
+Number.parseFloat("He was 40"); // NaN
+
+------------------------
+// 04. parseInt()
+// The Number.parselnt() method parses a value as a string and returns the first integer.
+// If the first character cannot be converted, NaN is returned. Leading and trailing spaces are ignored. 
+// Only the first integer found is returned
+
+// Syntax
+Number.parseInt (value) ;
+
+// Examples
+Number.parseInt("10");        // 10
+Number.parseInt("10.00");     // 10
+Number.parseInt("10. 33 ");   // 10
+Number.parseInt("34 45 66");  // 34
+Number.parseInt(" 60 ");      // 60
+Number.parseInt("40 years");  // 40
+Number.parseInt("He was 40"); // NaN
+
+
+------------------------
+
+// Part II: Number Methods
+// 01. toExponential()
 // The toExponential() method converts a number into an exponential notation. 
 // It is a "shorthand" way of writing repeated multiplication.
 
@@ -195,6 +270,7 @@ let n = num.toExponential(3);   // 5.568e+0
 
 ------------------------
 
+// 02. toFixed()
 // The toFixed() method converts a number to a string. 
 // and this method rounds the string to a specified number of decimals.
 
@@ -207,6 +283,106 @@ let n = num.toFixed();   // 6
 // Example 2
 let num = 5.56789;
 let n = num.toFixed(2);   // 5.57
+
+----------------------------
+
+// 03. toPrecision()
+// The toPrecision() method formats a number to a specified length.
+// A decimal point and nulls are added (if needed), to create the specified length.
+
+// Syntax
+number.toPrecision(x);
+
+// Example 1
+let num = 13.3714;
+let n = num.toPrecision(2); // 13
+
+// Example 2
+let num = 0.001658853;
+num.toPrecision(2); // 0.0017
+num.toPrecision(3); // 0.00166
+num.toPrecision(10); // 0. 001658853000
+
+----------------------------
+
+// 04. ValueOf()
+// The valueof() method returns the primitive value of a number.
+
+// Syntax
+number.valueOf () ;
+// Example
+let num = 15;
+let n num.value0f() ; // 15
+
+----------------------------
+
+
+// Part III: Number Object Methods
+// 01. number.isInteger()
+// The Number.isInteger() method returns true if the argument is an integer.
+
+// Syntax
+number.isInteger(value);
+// Example
+Number.isInteger (10) ; //true
+Number.isInteger (10.5); //false
+
+----------------------------
+
+// 02. number.isSafeInteger()
+// A safe integer is an integer that can be exactly represented as a double precision number.
+// The Number.isSafelnteger() method returns true if the argument is a safe intege.
+
+// Syntax
+Number.isSafeInteger(value);
+
+// Example 1
+Number.isSafeInteger(10);                   // true
+
+// Example 2
+Number.isSafeInteger(12345678901234567890);  // false
+
+
+// 03. number.parseFloat()
+// The Number.parseFloat() parses a string and returns a number.
+// Spaces are allowed. Only the first number is returned: If the number cannot be converted, returns NaN.
+
+// Syntax
+Number.parseFloat(value);
+
+// Examples
+Number.parseFloat("10");        // 10
+Number.parseFloat("10.33");     // 10.33
+Number.parseFloat("10 20 30");  // 10
+Number.parseFloat("10 years");  // 10
+Number.parseFloat("years 10");  // NaN
+
+
+
+// 04. number.parseInt()
+// The Number.parselnt() parses a string and returns a whole number.
+// Spaces are allowed. Only the first number is returned:
+
+// Syntax
+Number.parseInt(value);
+
+// Examples
+Number.parseInt("-10");       // -10
+Number.parseInt("-10.33");    // -10
+Number.parseInt("10");        // 10
+Number.parseInt("10.33");     // 10
+Number.parseInt("10 20 30");  // 10
+Number.parseInt("10 years");  // 10
+Number.parseInt("years 10");  // NaN
+
+
+
+
+
+
+
+
+
 
 
 
